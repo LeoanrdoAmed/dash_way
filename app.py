63908ad90442865e3830166fa250123ec3c2a1d2
LeoanrdoAmed <leonardo.amed2@gmail.com>
2025-04-22 16:10:45 -0300

    Corrige execução do app.py para suportar porta do Render

--- app.py
+++ app.py
@@ -559,4 +559,6 @@ def iniciar_atualizacao(n_clicks):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
