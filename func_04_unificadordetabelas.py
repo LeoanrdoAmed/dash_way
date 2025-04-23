816f5e356bb7c392a436eefe460b55ab113a28b8
LeoanrdoAmed <leonardo.amed2@gmail.com>
2025-04-22 21:37:30 -0300

    atualizar render.yaml

--- func_04_unificadordetabelas.py
+++ func_04_unificadordetabelas.py
@@ -1,19 +1,19 @@
+import os
 import pandas as pd
 
-# Corrigir os caminhos de leitura dos arquivos JSON
-base_cc = "/data/base_01_cc.json"
-base_cb = "/data/base_02_cb.json"
-base_mv = "/data/base_03_mv.json"
-base_cr = "/data/base_05_cr.json"
+DATA_DIR = "/data" if os.path.exists("/data") else "data"
+os.makedirs(DATA_DIR, exist_ok=True)
 
-# Leitura
+base_cc = os.path.join(DATA_DIR, "base_01_cc.json")
+base_cb = os.path.join(DATA_DIR, "base_02_cb.json")
+base_mv = os.path.join(DATA_DIR, "base_03_mv.json")
+base_cr = os.path.join(DATA_DIR, "base_05_cr.json")
+base_final = os.path.join(DATA_DIR, "base_final_04_rc.json")
+
 df1 = pd.read_json(base_cc)
 df2 = pd.read_json(base_cb)
-df3 = pd.read_json(base_mv)
+df3 = pd.DataFrame([])  # Não implementado ainda
 df4 = pd.read_json(base_cr)
 
-# Unificação dos dados
-tb_rc_final = pd.concat([df1, df2, df3, df4], ignore_index=True)
-
-# Exportação do resultado final
-tb_rc_final.to_json("/data/base_final_04_rc.json", orient="records", indent=2)
+tb_rc_final = pd.concat([df1, df2, df4], ignore_index=True)
+tb_rc_final.to_json(base_final, orient="records", indent=2)
